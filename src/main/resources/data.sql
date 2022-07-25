f5b7776616837f5cab961f1113cb5abf9cccdb2b
Anderson Hsieh <andy13579246810@gmail.com>
2022-07-25 01:48:11 -0400

    add mock data for testing in data.sql, also added dbmigration properties in applicaiton.properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (1,'達創11111','PO customerOrderId',NULL,'2022-03-29','2022-07-24 22:05:15.000000','note11111111');
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (2,'千得 2','PO customerOrderId',NULL,'2022-03-29','2022-07-24 22:04:15.000000','note11111111');
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (3,'千得 2','PO customerOrderId',NULL,'2022-03-29','2022-07-24 23:32:35.000000','note11111111');
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (4,'千得 2','PO customerOrderId',NULL,'2022-03-29','2022-07-24 23:32:37.000000','note11111111');
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (5,'千得 2','PO customerOrderId',NULL,'2022-03-29','2022-07-24 23:32:38.000000','note11111111');
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (6,'千得 2','PO customerOrderId',NULL,'2022-03-29','2022-07-24 23:32:39.000000','note11111111');
+INSERT INTO `orders` (`order_id`,`customer_id`,`customer_order_id`,`delivered_at`,`due_date`,`issued_at`,`note`) VALUES (7,'千得 2','PO customerOrderId',NULL,'2022-03-29','2022-07-24 23:32:40.000000','note11111111');
+
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('24fabd51-2eb8-40f7-9a4f-6e14201433f5','complete new model 4',1000,NULL,11,7);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('2b7ae587-6089-4fe4-8242-879afb508e8d','complete new model 3',1000,NULL,11,2);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('3a3b3d7e-4297-485a-a012-f4d8c4bd5bdc','complete new model 3',1000,'test ticket id6',11,1);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('3e8e1456-1666-4f9e-8f55-8766f2212359','complete new model 3',1000,NULL,11,7);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('4cfa15c9-f066-400d-92ea-7eff69825d67','complete new model 4',1000,NULL,11,2);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('4f950d54-591d-49be-bb7a-a48950cbfa77','complete new model 4',1000,NULL,11,6);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('5475c400-3740-4936-8bb1-dcc6ba3ae33e','complete new model 4',1000,NULL,11,4);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('5b0b8ed9-5765-4f91-a420-32d6cb2bb906','complete new model 3',1000,NULL,11,3);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('6e865335-f864-4261-ae6d-0ed186e4714c','complete new model 3',1000,NULL,11,6);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('b2e50e28-2b6a-4ba9-8377-5cefce40395c','complete new model 4',1000,NULL,11,5);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('bef7fbb4-4962-4732-98ef-1f723074bba5','complete new model 3',1000,NULL,11,5);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('cdf50e10-05b6-4bee-820f-99a665f8a661','complete new model 3',1000,NULL,11,4);
+INSERT INTO `product_entries` (`product_entry_id`,`model`,`price`,`product_ticket_id`,`quantity`,`order_id_fk`) VALUES ('ee4b8e06-7bf9-4c71-a233-ee8afb27f6ee','complete new model 4',1000,NULL,11,3);
